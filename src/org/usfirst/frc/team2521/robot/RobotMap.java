5b3b919e472be70a5398f8e4f04b49b02101655e
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-01-13 17:49:24 -0800

    Fix talons

--- src/org/usfirst/frc/team2521/robot/RobotMap.java
+++ src/org/usfirst/frc/team2521/robot/RobotMap.java
@@ -4,8 +4,8 @@ public class RobotMap {
 	public static final int LEFT_STICK_PORT = 0;
 	public static final int RIGHT_STICK_PORT = 1;
 	
-	public static final int FRONT_RIGHT_MOTOR = 41;
-	public static final int FRONT_LEFT_MOTOR = 42;
-	public static final int REAR_RIGHT_MOTOR = 47;
-	public static final int REAR_LEFT_MOTOR = 48;
+	public static final int FRONT_RIGHT_MOTOR = 42;
+	public static final int FRONT_LEFT_MOTOR = 41;
+	public static final int REAR_RIGHT_MOTOR = 48;
+	public static final int REAR_LEFT_MOTOR = 47;
 }
