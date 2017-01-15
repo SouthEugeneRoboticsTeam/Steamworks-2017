354428684134dd1f2c2160aaf2ba7af62ae0c69e
Dani White <white.dani.m@gmail.com>
2017-01-15 14:24:40 -0800

    Change variable names

--- src/org/usfirst/frc/team2521/robot/commands/AutoAlign.java
+++ src/org/usfirst/frc/team2521/robot/commands/AutoAlign.java
@@ -13,8 +13,8 @@ public class AutoAlign extends PIDCommand {
 	private static final double I = 0;
 	private static final double D = 0;
 	
-	private static final double errorThreshold = 5;
-	private static final double lidarMaxDistance = 1100; // Is a minimum value (since lidar get smaller as distance gets larger)
+	private static final double ERROR_THRESHOLD = 5;
+	private static final double LIDAR_MAX_DISTANCE = 1100; // Is a minimum value (since lidar get smaller as distance gets larger)
 	
 	private boolean overShot = false; // Is true if the lidar is pointed off too far away (e.g., off from the airship)
 
@@ -38,12 +38,12 @@ public class AutoAlign extends PIDCommand {
         if (Robot.DEBUG) {
         	SmartDashboard.putNumber("Error threshold error", Math.abs(Robot.sensors.getLeftLidar()-Robot.sensors.getRightLidar()));
         }
-        if (Robot.sensors.getLeftLidar() < lidarMaxDistance || Robot.sensors.getRightLidar() < lidarMaxDistance) {
+        if (Robot.sensors.getLeftLidar() < LIDAR_MAX_DISTANCE || Robot.sensors.getRightLidar() < LIDAR_MAX_DISTANCE) {
         	SmartDashboard.putString("Auto align status", "Error: Lidar pointing off too far");
         	overShot = true;
         	return true;
         }
-    	return Math.abs(Robot.sensors.getLeftLidar()-Robot.sensors.getRightLidar()) < errorThreshold;
+    	return Math.abs(Robot.sensors.getLeftLidar()-Robot.sensors.getRightLidar()) < ERROR_THRESHOLD;
     }
 
     protected void end() {
