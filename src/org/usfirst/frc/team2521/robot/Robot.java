b1518395b1f6b99de592c724f9f047da1a9ac4d7
Dani White <white.dani.m@gmail.com>
2017-01-15 14:50:47 -0800

    Fix unnecessary SmartDashboard prints

--- src/org/usfirst/frc/team2521/robot/Robot.java
+++ src/org/usfirst/frc/team2521/robot/Robot.java
@@ -18,7 +18,7 @@ import org.usfirst.frc.team2521.robot.subsystems.*;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 
 	public static Drivetrain drivetrain;
 	public static Sensors sensors;
