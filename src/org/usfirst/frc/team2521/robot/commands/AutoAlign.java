86984d391d0cb4dfd389ee25f820baad409b2538
Dani White <white.dani.m@gmail.com>
2017-01-15 14:22:28 -0800

    Add code to deal with airship edge case
    Now the auto align command stops if the distance detected by the lidar
    is too large

--- src/org/usfirst/frc/team2521/robot/commands/AutoAlign.java
+++ src/org/usfirst/frc/team2521/robot/commands/AutoAlign.java
@@ -14,13 +14,18 @@ public class AutoAlign extends PIDCommand {
 	private static final double D = 0;
 	
 	private static final double errorThreshold = 5;
+	private static final double lidarMaxDistance = 1100; // Is a minimum value (since lidar get smaller as distance gets larger)
+	
+	private boolean overShot = false; // Is true if the lidar is pointed off too far away (e.g., off from the airship)
 
     public AutoAlign() {
     	super(P, I, D);
     	requires(Robot.drivetrain);
     }
 
-    protected void initialize() {}
+    protected void initialize() {
+    	SmartDashboard.putString("Auto align status", "No errors");
+    }
 
     protected void execute() {
     	if(Robot.DEBUG) {
@@ -33,10 +38,19 @@ public class AutoAlign extends PIDCommand {
         if (Robot.DEBUG) {
         	SmartDashboard.putNumber("Error threshold error", Math.abs(Robot.sensors.getLeftLidar()-Robot.sensors.getRightLidar()));
         }
+        if (Robot.sensors.getLeftLidar() < lidarMaxDistance || Robot.sensors.getRightLidar() < lidarMaxDistance) {
+        	SmartDashboard.putString("Auto align status", "Error: Lidar pointing off too far");
+        	overShot = true;
+        	return true;
+        }
     	return Math.abs(Robot.sensors.getLeftLidar()-Robot.sensors.getRightLidar()) < errorThreshold;
     }
 
-    protected void end() {}
+    protected void end() {
+    	if (!overShot) {
+    		SmartDashboard.putString("Auto align status", "Done");
+    	}
+    }
 
     protected void interrupted() {}
     
