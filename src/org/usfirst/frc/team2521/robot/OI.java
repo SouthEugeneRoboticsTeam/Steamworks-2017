362506d5f5e77db7a7e216c0de3dd94728bf4401
Alex Saveau <SUPERCILEX@users.noreply.github.com>
2017-01-15 18:24:00 -0800

    Cleanup (#11)
    * Move code into proper directory
    
    * Make OI.java thread safe and use getInstance method
    
    * Prevent reassigning joysticks
    
    * Remove unused code
    
    * Fix merge mistakes
    
    * Fix style, remove unused code, add @Override annotation, etc
    
    * Use tabs :cry:
    
    * Fix merge mistakes
    
    * Undo package changes

--- src/org/usfirst/frc/team2521/robot/OI.java
+++ src/org/usfirst/frc/team2521/robot/OI.java
@@ -1,51 +1,47 @@
 package org.usfirst.frc.team2521.robot;
 
+import org.usfirst.frc.team2521.robot.commands.AutoAlign;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-
-import org.usfirst.frc.team2521.robot.commands.*;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	private Joystick left;
-	private Joystick right;
+	private static OI instance;
+
+	private final Joystick left;
+	private final Joystick right;
 
 	private JoystickButton autoAlignButton;
-	
-	private static OI instance;
-	
-	public OI() {
+
+	private OI() {
 		left = new Joystick(RobotMap.LEFT_STICK_PORT);
 		right = new Joystick(RobotMap.RIGHT_STICK_PORT);
-		
+
 		initButtons();
 	}
-	
-	public static OI getInstance() {
-		if (instance == null) {
-			instance = new OI();
-		}
-		
-		return instance;
+
+	public synchronized static OI getInstance() {
+		return instance == null ? instance = new OI() : instance;
 	}
-	
+
 	public Joystick getLeftStick() {
 		return left;
 	}
-	
+
 	public Joystick getRightStick() {
 		return right;
 	}
 
 	public void initButtons() {
 		autoAlignButton = new JoystickButton(right, RobotMap.AUTO_ALIGN_BUTTON_PORT);
-		
+
 		tieButtons();
 	}
-	
+
 	public void tieButtons() {
 		autoAlignButton.whenPressed(new AutoAlign());
 	}
