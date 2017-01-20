3f6886f075d9153f08a767bdb22810fde7682faf
Dani White <white.dani.m@gmail.com>
2017-01-19 16:40:13 -0800

    Create DriveToGear command

--- src/org/usfirst/frc/team2521/robot/commands/DriveToGear.java
+++ src/org/usfirst/frc/team2521/robot/commands/DriveToGear.java
@@ -3,11 +3,13 @@ package org.usfirst.frc.team2521.robot.commands;
 import org.usfirst.frc.team2521.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class DriveToGear extends Command {
+	double targetAngle;
 
 	public DriveToGear() {
 		requires(Robot.drivetrain);
@@ -15,13 +17,21 @@ public class DriveToGear extends Command {
 
 	protected void initialize() {}
 
-	protected void execute() {}
+	protected void execute() {
+		double alpha = Robot.sensors.getAngleFromCamToWallPlane();
+		SmartDashboard.putNumber("Alpha", alpha);
+		double beta = Robot.sensors.getAngleFromCamToVisionTarget();
+		targetAngle = 0.5*alpha + beta;
+		SmartDashboard.putNumber("Target angle", targetAngle);
+	}
 
 	protected boolean isFinished() {
 		return false;
 	}
 
-	protected void end() {}
+	protected void end() {
+		SmartDashboard.putNumber("Target angle", targetAngle);
+	}
 
 	protected void interrupted() {
 	}
