ea2fc9497f7bd498e4da5183ad64146184e31016
Andrew Dassonville <andrewda@users.noreply.github.com>
2017-01-23 17:42:21 -0800

    Add shooter PID (#17)
    * Add Shooter subsystem for prototypes
    
    * Tuned PID constants for smooth initial approach to setpoint, but bumpy correction after the shot
    
    * Change constants for better rebound after shot, fix motor signs

--- src/org/usfirst/frc/team2521/robot/Robot.java
+++ src/org/usfirst/frc/team2521/robot/Robot.java
@@ -2,25 +2,24 @@ package org.usfirst.frc.team2521.robot;
 
 import org.usfirst.frc.team2521.robot.subsystems.Drivetrain;
 import org.usfirst.frc.team2521.robot.subsystems.Sensors;
+import org.usfirst.frc.team2521.robot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 	public static final boolean DEBUG = false;
 
 	public static Drivetrain drivetrain;
 	public static Sensors sensors;
-
-	public static OI oi;
+	public static Shooter shooter;
 
 	@Override
 	public void robotInit() {
 		drivetrain = new Drivetrain();
 		sensors = new Sensors();
-
-		oi = OI.getInstance();
 	}
 
 	@Override
---
+++ src/org/usfirst/frc/team2521/robot/subsystems/Shooter.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team2521.robot.subsystems;
+
+import com.ctre.CANTalon;
+
+import org.usfirst.frc.team2521.robot.RobotMap;
+import org.usfirst.frc.team2521.robot.commands.PIDShoot;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Shooter extends Subsystem {
+	private CANTalon left;
+	private CANTalon right;
+
+	public Shooter() {
+		left = new CANTalon(RobotMap.LEFT_SHOOT_MOTOR);
+		right = new CANTalon(RobotMap.RIGHT_SHOOT_MOTOR);
+	}
+
+	public void setMotor(double value) {
+		left.set(value);
+		right.set(-value);
+	}
+
+	public double getEncVelocity() {
+		return left.getEncVelocity();
+	}
+
+	@Override
+	public void initDefaultCommand() {
+		setDefaultCommand(new PIDShoot());
+	}
+}
