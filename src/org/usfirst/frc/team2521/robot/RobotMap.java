b1dc5731d05de62ed6bd6eccc149eb6c9cbb3824
Jamdan2 <jmdn2message@gmail.com>
2017-01-24 17:31:43 -0800

    Setup basic climber subsystem (#18)
    * Setup basic climber subsystem
    
    * Cleanup code
    
    Fixes #5

--- src/org/usfirst/frc/team2521/robot/RobotMap.java
+++ src/org/usfirst/frc/team2521/robot/RobotMap.java
@@ -6,6 +6,7 @@ package org.usfirst.frc.team2521.robot;
 public class RobotMap {
 	public static final int LEFT_STICK_PORT = 0;
 	public static final int RIGHT_STICK_PORT = 1;
+	public static final int SECONDARY_STICK_PORT = 2;
 
 	public static final int AUTO_ALIGN_BUTTON_PORT = 3; // On right stick
 
@@ -13,6 +14,8 @@ public class RobotMap {
 	public static final int FRONT_LEFT_MOTOR = 41;
 	public static final int REAR_RIGHT_MOTOR = 48;
 	public static final int REAR_LEFT_MOTOR = 47;
+	
+	public static final int CLIMBER_WHEEL_MOTOR = 45;
 
 	public static final int LEFT_LIDAR_PORT = 1;
 	public static final int RIGHT_LIDAR_PORT = 0;
