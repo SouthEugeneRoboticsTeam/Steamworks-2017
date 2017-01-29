ea003fd2c24c39e32166d3a9ab749189776e0412
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-01-29 09:29:37 -0800

    Add command descriptions

--- src/org/usfirst/frc/team2521/robot/commands/AutoAlign.java
+++ src/org/usfirst/frc/team2521/robot/commands/AutoAlign.java
@@ -6,7 +6,8 @@ import edu.wpi.first.wpilibj.command.PIDCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * Auto aligns the drivetrain to be straight relative to a wall using lidar.
+ * Automatically aligns the drivetrain to be straight relative to a wall using
+ * lidar
  */
 public class AutoAlign extends PIDCommand {
 	private static final double P = 0.005;
@@ -14,18 +15,13 @@ public class AutoAlign extends PIDCommand {
 	private static final double D = 0;
 
 	private static final double ERROR_THRESHOLD = 5;
-	/**
-	 * Minimum value (since lidar get smaller as distance gets larger)
-	 */
 	private static final double LIDAR_MAX_DISTANCE = 1100;
 
-	/**
-	 * True if the lidar is pointed off too far away (e.g., off from the airship)
-	 */
 	private boolean overShot = false;
 
 	public AutoAlign() {
 		super(P, I, D);
+
 		requires(Robot.drivetrain);
 	}
 
@@ -36,18 +32,21 @@ public class AutoAlign extends PIDCommand {
 
 	@Override
 	protected boolean isFinished() {
+		// Check if we're too far away
 		if (Robot.sensors.getLeftLidar() < LIDAR_MAX_DISTANCE || Robot.sensors.getRightLidar() < LIDAR_MAX_DISTANCE) {
 			SmartDashboard.putString("Auto align status", "Error: Lidar pointing off too far");
 			overShot = true;
 			return true;
 		}
 
+		// Finish the command if we're within our error threshold
 		return Math.abs(Robot.sensors.getLeftLidar() - Robot.sensors.getRightLidar()) < ERROR_THRESHOLD;
 	}
 
 	@Override
 	protected void end() {
-		if (!overShot) SmartDashboard.putString("Auto align status", "Done");
+		if (!overShot)
+			SmartDashboard.putString("Auto align status", "Done");
 	}
 
 	@Override
@@ -57,7 +56,8 @@ public class AutoAlign extends PIDCommand {
 
 	@Override
 	protected void usePIDOutput(double output) {
-		if (Robot.DEBUG) SmartDashboard.putNumber("Auto align output", output);
+		if (Robot.DEBUG)
+			SmartDashboard.putNumber("Auto align output", output);
 
 		Robot.drivetrain.setLeft(output);
 		Robot.drivetrain.setRight(-output);
--- src/org/usfirst/frc/team2521/robot/commands/TeleopDrivetrain.java
+++ src/org/usfirst/frc/team2521/robot/commands/TeleopDrivetrain.java
@@ -4,6 +4,9 @@ import org.usfirst.frc.team2521.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
+/**
+ * Allows a driver to control the drivetrain during teleop
+ */
 public class TeleopDrivetrain extends Command {
 	public TeleopDrivetrain() {
 		requires(Robot.drivetrain);
