40d789b3b9dccc9111ca8ee82b9e8ddd1ca655b4
Dani White <white.dani.m@gmail.com>
2017-01-29 16:23:48 -0800

    Move drive to gear code into a PID loop

--- src/org/usfirst/frc/team2521/robot/commands/DriveToGear.java
+++ src/org/usfirst/frc/team2521/robot/commands/DriveToGear.java
@@ -2,41 +2,35 @@ package org.usfirst.frc.team2521.robot.commands;
 
 import org.usfirst.frc.team2521.robot.Robot;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.PIDCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Command for driving to the gear automatically
  */
-public class DriveToGear extends Command {
-	private static final double P = 0.002;
+public class DriveToGear extends PIDCommand {
+	private static final double P = 0.005;
 	private static final double I = 0;
 	private static final double D = 0;
 	
+	private static final double STRAIGHT_SPEED = 0.3;
+	/**
+	 * Speed to go at once we're centered on the target
+	 */
+	
+	private static final double OFFSET_CUTOFF = 20;
+	
 	public DriveToGear() {
-		//super(P, I, D);
+		super(P, I, D);
 		requires(Robot.drivetrain);
 	}
 	
-	@Override
-	protected void execute() {
-		if (Math.abs(Robot.sensors.getCVOffsetX()) < 20) {
-			Robot.drivetrain.setLeft(-.3);
-			Robot.drivetrain.setRight(.3);
-		}
-		
-		if (Robot.sensors.getCVOffsetX() < 0) {
-			Robot.drivetrain.setLeft(-.2);
-		} else {
-			Robot.drivetrain.setRight(.2);
-		}
-	}
 
 	@Override
 	protected boolean isFinished() {
 		return false;
 	}
-	/*
+	
 	@Override
 	protected double returnPIDInput() {
 		return Robot.sensors.getCVOffsetX();
@@ -44,20 +38,24 @@ public class DriveToGear extends Command {
 
 	@Override
 	protected void usePIDOutput(double output) {
-		if (Robot.DEBUG) SmartDashboard.putNumber("Drive to gear output", output);
-
-		if (Math.abs(Robot.sensors.getCVOffsetX()) < 10) {
-			Robot.drivetrain.setLeft(.5);
-			Robot.drivetrain.setRight(.5);
+		SmartDashboard.putNumber("Drive to gear output", output);
+		if (Math.abs(output) > .3) {
+			output = .3 * Math.signum(output);
+		}
+		if (Math.abs(Robot.sensors.getCVOffsetX()) < OFFSET_CUTOFF) {
+			Robot.drivetrain.setLeft(-STRAIGHT_SPEED);
+			Robot.drivetrain.setRight(STRAIGHT_SPEED);
 		}
 		
 		// If CV offset is positive, we're too far left. If it's negative, we're too far right
 		// This way we also are always moving forward
-		if (Robot.sensors.getCVOffsetX() > 0) {
-			Robot.drivetrain.setLeft(output);
+		if (Robot.sensors.getCVOffsetX() < 0) {
+			Robot.drivetrain.setLeft(-output);
+			SmartDashboard.putNumber("Left", -output);
 		} else {
-			Robot.drivetrain.setRight(output);
+			Robot.drivetrain.setRight(-output);
+			SmartDashboard.putNumber("Right", -output);
 		}
 	}
-	*/
+
 }
