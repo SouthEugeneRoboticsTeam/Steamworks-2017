380193e71f8629c695025166880b5ec163605961
Dani White <white.dani.m@gmail.com>
2017-02-07 20:04:40 -0800

    Add ultrasonics

--- src/org/usfirst/frc/team2521/robot/subsystems/Sensors.java
+++ src/org/usfirst/frc/team2521/robot/subsystems/Sensors.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Sensors extends Subsystem {
 	private AnalogInput frontUltra;
 	private AnalogInput sideUltra;
-	
+
 	private NetworkTable table;
 	
 	private AHRS ahrs;
@@ -64,7 +64,6 @@ public class Sensors extends Subsystem {
 	
 	public double getFrontUltraInches() {
 		return frontUltra.getVoltage() * 1000 / 9.8;
-
 	}
 
 	/**
