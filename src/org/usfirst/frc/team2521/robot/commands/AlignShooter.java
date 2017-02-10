fb74905f23c81a41ea3d3811eab2b98e4fe69e27
Dani White <white.dani.m@gmail.com>
2017-02-09 18:42:23 -0800

    Incorporate the correct geometry

--- src/org/usfirst/frc/team2521/robot/commands/AlignShooter.java
+++ src/org/usfirst/frc/team2521/robot/commands/AlignShooter.java
@@ -3,16 +3,22 @@ package org.usfirst.frc.team2521.robot.commands;
 import org.usfirst.frc.team2521.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class AlignShooter extends CommandGroup {
+	private static final double X_SETPOINT = 21;
+	private static final double ROBOT_HALF_WIDTH = 15.75;
+	private static final double SIDE_ULTRA_OFFSET = 19;
+	private static final double ROBOT_LENGTH = 26;
 
 	public AlignShooter() {
 		double dist = Robot.sensors.getSideUltraInches();
-		dist += 15.75;
-		dist -= 19;
+		SmartDashboard.putNumber("Raw distance",dist);
+		dist = Math.sqrt(2) + dist + ROBOT_HALF_WIDTH - SIDE_ULTRA_OFFSET - 1.5*ROBOT_LENGTH;
+		SmartDashboard.putNumber("Setpoint",dist);
 		addSequential(new DriveToUltra(dist));
 		addSequential(new TurnToAngle(-45));
 		addSequential(new DriveToUltra(0));
