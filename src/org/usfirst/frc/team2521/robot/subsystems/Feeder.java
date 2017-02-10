60c828bfad634ff0089bf257a2c92abdc407f1df
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-02-09 21:48:26 -0800

    Add documentation to feeder subsystem

--- src/org/usfirst/frc/team2521/robot/subsystems/Feeder.java
+++ src/org/usfirst/frc/team2521/robot/subsystems/Feeder.java
@@ -6,6 +6,10 @@ import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+/**
+ * Feeder is the subsystem dedicated to the shooter's feeding system which
+ * delivers balls from the indexer to the fly wheel.
+ */
 public class Feeder extends Subsystem {
 	private CANTalon feeder;
 
@@ -13,6 +17,11 @@ public class Feeder extends Subsystem {
 		feeder = new CANTalon(RobotMap.FEEDER_MOTOR);
 	}
 
+	/**
+	 * Sets the feeder wheel to a certain value.
+	 *
+	 * @param value  the value at which to set the feeder wheel
+	 */
 	public void setMotor(double value) {
 		feeder.set(-value);
 	}
