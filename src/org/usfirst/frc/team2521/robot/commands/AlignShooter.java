11fce3c1a65c4f77fa17170691522bc9e4c7d27d
Dani White <white.dani.m@gmail.com>
2017-02-11 12:48:45 -0800

    Change align shooter calculation

--- src/org/usfirst/frc/team2521/robot/commands/AlignShooter.java
+++ src/org/usfirst/frc/team2521/robot/commands/AlignShooter.java
@@ -11,13 +11,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class AlignShooter extends CommandGroup {
 	private static final double X_SETPOINT = 21;
 	private static final double ROBOT_HALF_WIDTH = 15.75;
-	private static final double SIDE_ULTRA_OFFSET = 19;
+	private static final double SIDE_ULTRA_OFFSET = 19.5;
 	private static final double ROBOT_LENGTH = 26;
 
 	public AlignShooter() {
 		double dist = Robot.sensors.getSideUltraInches();
 		SmartDashboard.putNumber("Raw distance",dist);
-		dist = Math.sqrt(2) + dist + ROBOT_HALF_WIDTH - SIDE_ULTRA_OFFSET - 1.5*ROBOT_LENGTH;
+		dist = X_SETPOINT*Math.sqrt(2) + dist + ROBOT_HALF_WIDTH - SIDE_ULTRA_OFFSET - 0.5*ROBOT_LENGTH;
 		SmartDashboard.putNumber("Setpoint",dist);
 		addSequential(new DriveToUltra(dist));
 		addSequential(new TurnToAngle(-45));
