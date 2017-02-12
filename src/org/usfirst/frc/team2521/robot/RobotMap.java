569a1be41013bbd1baad917387cd7877f56b0d53
Dani White <white.dani.m@gmail.com>
2017-02-12 12:30:12 -0800

    Create shooter align (#32)
    * Reset angle before turning with navx PID
    
    * Change align shooter calculation
    
    * Tune turn to angle, incorporate lidar

--- src/org/usfirst/frc/team2521/robot/RobotMap.java
+++ src/org/usfirst/frc/team2521/robot/RobotMap.java
@@ -10,7 +10,7 @@ public class RobotMap {
 
 	public static final int SPIN_FLYWHEEL_BUTTON_PORT = 1; // On right stick
 	public static final int SPIN_FEEDER_BUTTON_PORT = 2; // On right stick
-	public static final int AUTO_ALIGN_BUTTON_PORT = 3; // On right stick
+	public static final int ALIGN_SHOOTER_BUTTON = 3; // On right stick
 	public static final int DRIVE_TO_GEAR_LEFT_PORT = 4; // On right stick
 	public static final int DRIVE_TO_GEAR_RIGHT_PORT = 5; // On right stick
 
@@ -24,6 +24,6 @@ public class RobotMap {
 	public static final int SHOOTER_MOTOR = 43;
 	public static final int FEEDER_MOTOR = 31;
 
-	public static final int LEFT_LIDAR_PORT = 1;
-	public static final int RIGHT_LIDAR_PORT = 0;
+	public static final int BACK_ULTRA_PORT = 1;
+	public static final int SIDE_LIDAR_PORT = 2;
 }
---
+++ src/org/usfirst/frc/team2521/robot/commands/DriveToUltra.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team2521.robot.commands;
+
+import org.usfirst.frc.team2521.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class DriveToUltra extends Command {
+	private final static double ERROR_THRESHOLD = 1;
+
+	// How far from the right side of the boiler the robot's center should be
+	private static final double X_SETPOINT = 21;
+
+	private static final double ROBOT_HALF_WIDTH = 15.75;
+
+	// How far the side ultrasonic is from front of the robot
+	private static final double SIDE_ULTRA_OFFSET = 19.5;
+
+	private static final double ROBOT_LENGTH = 26;
+	
+	private double setpoint;
+	
+	// `true` if we should calculate setpoint using align shooter formula
+	private boolean isAlignShooter;
+
+	public DriveToUltra(double setpoint, boolean isAlignShooter) {
+		this.setpoint = setpoint;
+		this.isAlignShooter = isAlignShooter;
+	}
+	
+	protected void initialize() {
+		if (isAlignShooter) {
+			setpoint = Robot.sensors.getSideLidarInches() + X_SETPOINT*Math.sqrt(2) + ROBOT_HALF_WIDTH - SIDE_ULTRA_OFFSET - 0.5*ROBOT_LENGTH;
+		}
+	}
+
+	protected void execute() {
+		Robot.drivetrain.setLeft((setpoint - Robot.sensors.getFrontUltraInches() > 0) ? .2 : -.2);
+		Robot.drivetrain.setRight((setpoint - Robot.sensors.getFrontUltraInches() > 0) ? -.2 : .2);
+	}
+
+	protected boolean isFinished() {
+		if (Robot.DEBUG) {
+			SmartDashboard.putNumber("Drive to ultra setpoint", setpoint);
+			SmartDashboard.putNumber("Drive to ultra error", setpoint - Robot.sensors.getFrontUltraInches());
+		}
+		return Math.abs(setpoint - Robot.sensors.getFrontUltraInches()) < ERROR_THRESHOLD;
+	}
+}
