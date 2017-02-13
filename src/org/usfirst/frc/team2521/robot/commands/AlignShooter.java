25182ff8ecc658d3e615aa81d5390957a561dc25
Alex Saveau <saveau.alexandre@gmail.com>
2017-02-12 20:07:09 -0800

    Refactor AlignShooter classes into proper Java

--- src/org/usfirst/frc/team2521/robot/commands/AlignShooter.java
+++ src/org/usfirst/frc/team2521/robot/commands/AlignShooter.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2521.robot.commands;
 
+import org.usfirst.frc.team2521.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -7,7 +9,20 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class AlignShooter extends CommandGroup {
 	public AlignShooter() {
-		addSequential(new AlignShooterDrive());
+		addSequential(new DriveToUltra(-1, true) {
+			/** How far from the right side of the boiler the robot's center */
+			private static final double X_SETPOINT = 21;
+			private static final double ROBOT_HALF_WIDTH = 15.75;
+			/** How far the side ultrasonic is from front of the robot */
+			private static final double SIDE_ULTRA_OFFSET = 19.5;
+			private static final double ROBOT_LENGTH = 26;
+
+			@Override
+			protected void initialize() {
+				setpoint = Robot.sensors.getSideLidarInches() + X_SETPOINT * Math.sqrt(2) + ROBOT_HALF_WIDTH
+						- SIDE_ULTRA_OFFSET - 0.5 * ROBOT_LENGTH;
+			}
+		});
 		addSequential(new TurnToAngle(-45));
 		addSequential(new DriveToUltra(0, true));
 	}
--- src/org/usfirst/frc/team2521/robot/commands/DriveToUltra.java
+++ src/org/usfirst/frc/team2521/robot/commands/DriveToUltra.java
@@ -11,10 +11,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class DriveToUltra extends Command {
 	private final static double ERROR_THRESHOLD = 1;
 
+	protected double setpoint;
 	private double ultrasonicValue;
-	double setpoint;
-
-	// `true` if we should use the front ultrasonic
+	/** {@code true} if we should use the front ultrasonic */
 	private boolean useRearUltra = false;
 
 	public DriveToUltra(double setpoint, boolean useRearUltra) {
@@ -22,18 +21,20 @@ public class DriveToUltra extends Command {
 		this.useRearUltra = useRearUltra;
 	}
 
+	@Override
 	protected void execute() {
 		ultrasonicValue = useRearUltra ? Robot.sensors.getRearUltraInches() : Robot.sensors.getFrontUltraInches();
 
 		if (useRearUltra) {
-			Robot.drivetrain.setLeft((setpoint - ultrasonicValue > 0) ? .2 : -.2);
-			Robot.drivetrain.setRight((setpoint - ultrasonicValue > 0) ? -.2 : .2);
+			Robot.drivetrain.setLeft(setpoint - ultrasonicValue > 0 ? .2 : -.2);
+			Robot.drivetrain.setRight(setpoint - ultrasonicValue > 0 ? -.2 : .2);
 		} else {
-			Robot.drivetrain.setLeft((setpoint - ultrasonicValue > 0) ? -.2 : .2);
-			Robot.drivetrain.setRight((setpoint - ultrasonicValue > 0) ? .2 : -.2);
+			Robot.drivetrain.setLeft(setpoint - ultrasonicValue > 0 ? -.2 : .2);
+			Robot.drivetrain.setRight(setpoint - ultrasonicValue > 0 ? .2 : -.2);
 		}
 	}
 
+	@Override
 	protected boolean isFinished() {
 		if (Robot.DEBUG) {
 			SmartDashboard.putNumber("Drive to ultra setpoint", setpoint);
