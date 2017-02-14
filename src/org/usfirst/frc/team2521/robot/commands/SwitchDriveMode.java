284cf03dbd63fdf4c17704b9a36941c84b070041
Dani White <white.dani.m@gmail.com>
2017-02-13 18:07:21 -0800

    Add arcade drive
    Fixes #27

---
+++ src/org/usfirst/frc/team2521/robot/commands/SwitchDriveMode.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team2521.robot.commands;
+
+import org.usfirst.frc.team2521.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SwitchDriveMode extends Command {
+
+	@Override
+	protected void initialize() {
+		Robot.drivetrain.isArcade = !Robot.drivetrain.isArcade;
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
