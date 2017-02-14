760a8e635118e72e8676c95452a2792e8beb8522
Dani White <white.dani.m@gmail.com>
2017-02-13 17:07:07 -0800

    Change PID

--- src/org/usfirst/frc/team2521/robot/commands/TurnToAngle.java
+++ src/org/usfirst/frc/team2521/robot/commands/TurnToAngle.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class TurnToAngle extends PIDCommand {
-	private static final double P = 0.005;
+	private static final double P = 0.003;
 	private static final double I = 0;
 	private static final double D = 0;
 
