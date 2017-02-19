00d1991480c4cd6eb594d6709c6b3987203bd080
Alex Saveau <saveau.alexandre@gmail.com>
2017-02-18 18:39:23 -0800

    Make arcade mode default

--- src/org/usfirst/frc/team2521/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team2521/robot/subsystems/Drivetrain.java
@@ -17,12 +17,11 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * input.
  */
 public class Drivetrain extends Subsystem {
+	/** Speed to set drivetrain to when we want to move at a slow, constant speed */
+	public static final double SLOW_SPEED = 0.2;
 	private RobotDrive frontDrive;
 	private RobotDrive rearDrive;
 	private CANTalon frontLeft, frontRight, rearLeft, rearRight;
-	
-	/** Speed to set drivetrain to when we want to move at a slow, constant speed */
-	public static final double SLOW_SPEED = 0.2;
 
 	public Drivetrain() {
 		frontLeft = new CANTalon(RobotMap.FRONT_LEFT_MOTOR);
@@ -72,12 +71,11 @@ public class Drivetrain extends Subsystem {
 		rearLeft.changeControlMode(TalonControlMode.PercentVbus);
 		rearRight.changeControlMode(TalonControlMode.PercentVbus);
 
-		if (Preferences.getInstance().getBoolean("is_tank_mode", true)) {
-			tankDrive();
-		} else {
+		if (Preferences.getInstance().getBoolean("is_arcade_mode", true)) {
 			arcadeDrive();
+		} else {
+			tankDrive();
 		}
-
 	}
 
 	/**
