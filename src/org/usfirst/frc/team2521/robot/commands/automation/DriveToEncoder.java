15f971c5d8cfc55aef33efa6683d3390c37d15b6
Dani White <white.dani.m@gmail.com>
2017-02-18 20:12:48 -0800

    Clean up DriveToEncoder

--- src/org/usfirst/frc/team2521/robot/commands/automation/DriveToEncoder.java
+++ src/org/usfirst/frc/team2521/robot/commands/automation/DriveToEncoder.java
@@ -3,30 +3,24 @@ package org.usfirst.frc.team2521.robot.commands.automation;
 import org.usfirst.frc.team2521.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveToEncoder extends Command {
-	private final double ENC_PULSES_PER_REVOLUTION = 8129;
+	private final double ENC_PULSES_PER_REVOLUTION = 8192;
 	private final double WHEEL_DIAMETER = 6;
-
-	private double leftDistance;
-	private double rightDistance;
+	
+	private double distance;
 
 	public DriveToEncoder(double distance) {
 		requires(Robot.drivetrain);
 
 		distance = (distance / (WHEEL_DIAMETER * Math.PI)) * ENC_PULSES_PER_REVOLUTION;
-		distance = 1000;
-		SmartDashboard.putNumber("Left initial", Robot.drivetrain.getLeftPosition());
-		SmartDashboard.putNumber("Right initial", Robot.drivetrain.getRightPosition());
-		leftDistance = distance + Robot.drivetrain.getLeftPosition();
-		SmartDashboard.putNumber("Left setpoint", leftDistance);
-		rightDistance = distance + Robot.drivetrain.getRightPosition();
-		SmartDashboard.putNumber("Right setpoint", rightDistance);
+		distance = distance + Robot.drivetrain.getRightPosition();
+		
+		this.distance = distance;
 	}
 
 	protected void execute() {
-		Robot.drivetrain.setPosition(rightDistance);
+		Robot.drivetrain.setPosition(distance);
 	}
 
 	@Override
