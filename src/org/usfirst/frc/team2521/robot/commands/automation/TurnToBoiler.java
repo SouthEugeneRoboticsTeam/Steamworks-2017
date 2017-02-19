0ccbb2e479d0065f5543664cf043bfab3838c6bf
Dani White <white.dani.m@gmail.com>
2017-02-19 15:00:58 -0800

    Fix PID

--- src/org/usfirst/frc/team2521/robot/commands/automation/TurnToBoiler.java
+++ src/org/usfirst/frc/team2521/robot/commands/automation/TurnToBoiler.java
@@ -2,11 +2,10 @@ package org.usfirst.frc.team2521.robot.commands.automation;
 
 import org.usfirst.frc.team2521.robot.Robot;
 
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.PIDCommand;
 
 public class TurnToBoiler extends PIDCommand {
-	private static final double P = 0.008;
+	private static final double P = 0.002;
 	private static final double I = 0;
 	private static final double D = 0;
 	
