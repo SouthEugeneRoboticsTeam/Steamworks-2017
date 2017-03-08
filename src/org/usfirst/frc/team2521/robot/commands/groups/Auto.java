c9a1f940aa4207e7fb68af366e9a0f3b7329e0f5
Dani White <white.dani.m@gmail.com>
2017-03-07 19:15:19 -0800

    Add drive forward after gear auto

--- src/org/usfirst/frc/team2521/robot/commands/groups/Auto.java
+++ src/org/usfirst/frc/team2521/robot/commands/groups/Auto.java
@@ -9,20 +9,25 @@ import org.usfirst.frc.team2521.robot.subsystems.Sensors;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.TimedCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Auto extends CommandGroup {
 	public Auto() {
 		switch (OI.getInstance().getAutoMode()) {
 			case RobotMap.AutoModes.NOTHING:
+				SmartDashboard.putString("Auto mode", "Nothing");
 				break;
 			case RobotMap.AutoModes.CROSS_BASE_LINE:
+				SmartDashboard.putString("Auto mode", "Cross baseline");
 				addSequential(new RunDrivetrain(), 2);
 				break;
 			case RobotMap.AutoModes.BALLS_ONLY:
+				SmartDashboard.putString("Auto mode", "Balls only");
 				addSequential(new RunDrivetrain(), 1);
 				addSequential(new AlignShooter());
 				break;
 			case RobotMap.AutoModes.BALL_THEN_GEAR:
+				SmartDashboard.putString("Auto mode", "Ball then gear");
 				Robot.sensors.setCVCamera(Sensors.Camera.FRONT);
 				addSequential(new RunDrivetrain(), 1);
 				addSequential(new DriveToGear(false));
@@ -31,13 +36,17 @@ public class Auto extends CommandGroup {
 				addSequential(new AlignShooter());
 				break;
 			case RobotMap.AutoModes.GEAR_LEFT:
-				addSequential(new RunDrivetrain(), 1);
+				SmartDashboard.putString("Auto mode", "Gear left");
+				addSequential(new RunDrivetrain(), 2);
 				addSequential(new DriveToGear(false));
+				addSequential(new RunDrivetrain(), .2);
 				break;
 			case RobotMap.AutoModes.GEAR_MIDDLE:
+				SmartDashboard.putString("Auto mode", "Gear middle");
 				addSequential(new DriveToGear(false));
 				break;
 			case RobotMap.AutoModes.GEAR_RIGHT:
+				SmartDashboard.putString("Auto mode", "Gear right");
 				addSequential(new RunDrivetrain(), 1);
 				addSequential(new DriveToGear(true));
 				break;
