e9942f76b81bf6952d837fffb4fea4bee953a74c
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-03-23 19:34:46 -0700

    Fix climber direction

--- src/org/usfirst/frc/team2521/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team2521/robot/subsystems/Climber.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * Climber is the subsystem dedicated to the robot's rope climber.
  */
 public class Climber extends Subsystem {
-	private static final double CLIMBER_SPEED = 1;
+	private static final double CLIMBER_SPEED = -1;
 
 	private CANTalon master;
 	private CANTalon slave;
