0359ca1335cc5046db7bac0fb88d23b2b04aafb7
Alexandre Saveau <saveau.alexandre@gmail.com>
2017-03-23 19:53:03 -0700

    Make it clearer that DriveToBoiler.java isn't using DriveToBlob

--- src/org/usfirst/frc/team2521/robot/commands/automation/DriveToGear.java
+++ src/org/usfirst/frc/team2521/robot/commands/automation/DriveToGear.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team2521.robot.commands.automation;
 import org.usfirst.frc.team2521.robot.Robot;
 import org.usfirst.frc.team2521.robot.subsystems.Drivetrain;
 
+import edu.wpi.first.wpilibj.command.PIDCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import static org.usfirst.frc.team2521.robot.subsystems.Sensors.Camera;
@@ -10,18 +11,38 @@ import static org.usfirst.frc.team2521.robot.subsystems.Sensors.Camera;
 /**
  * This command drives to the gear drop-off automatically.
  */
-public class DriveToGear extends DriveToBlob {
+public class DriveToGear extends PIDCommand {
 	private static final double P = 0.008;
 	private static final double I = 0;
 	private static final double D = 0;
-	private boolean hasFoundBlob = false;
+
+	private static final double CAMERA_PROJ_PLANE_DISTANCE = 216.226;
+
+	/** true if we're straight-on */
+	private boolean oriented;
+	private boolean hasFoundBlob;
 
 	public DriveToGear() {
 		super(P, I, D);
+		requires(Robot.drivetrain);
 	}
 
-	private double getSlowSpeed() {
-		return Drivetrain.SLOW_SPEED;
+	@Override
+	protected void execute() {
+		// Angle between camera line of sight and target
+		Double offsetX = Robot.sensors.getCVOffsetX();
+		if (offsetX == null) return;
+
+		double targetAngle = Math.atan(offsetX / CAMERA_PROJ_PLANE_DISTANCE);
+
+		// Convert angle to degrees
+		targetAngle *= 180 / Math.PI;
+
+		// Consider ourselves oriented when our angle error is less than 20
+		oriented = Math.abs(targetAngle) < 20;
+
+		targetAngle += Robot.sensors.getNavxAngle();
+		setSetpoint(targetAngle);
 	}
 
 	@Override
@@ -44,8 +65,8 @@ public class DriveToGear extends DriveToBlob {
 			hasFoundBlob = true;
 			// If we are already oriented, drive straight
 			if (oriented) {
-				Robot.drivetrain.setLeft(getSlowSpeed());
-				Robot.drivetrain.setRight(getSlowSpeed());
+				Robot.drivetrain.setLeft(Drivetrain.SLOW_SPEED);
+				Robot.drivetrain.setRight(Drivetrain.SLOW_SPEED);
 			} else if (output > 0) {
 				Robot.drivetrain.setLeft(output);
 			} else {
@@ -53,8 +74,8 @@ public class DriveToGear extends DriveToBlob {
 			}
 		} else {
 			if (hasFoundBlob) {
-				Robot.drivetrain.setLeft(getSlowSpeed());
-				Robot.drivetrain.setRight(getSlowSpeed());
+				Robot.drivetrain.setLeft(Drivetrain.SLOW_SPEED);
+				Robot.drivetrain.setRight(Drivetrain.SLOW_SPEED);
 			}
 		}
 	}
