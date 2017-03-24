c4e67369f8a00594b05530d5fc95f8bfb6b313b9
Alexandre Saveau <saveau.alexandre@gmail.com>
2017-03-24 08:46:43 -0700

    Fix crash because CameraLooper doesn't require Camera

--- src/org/usfirst/frc/team2521/robot/commands/automation/camera/CameraLooper.java
+++ src/org/usfirst/frc/team2521/robot/commands/automation/camera/CameraLooper.java
@@ -1,8 +1,14 @@
 package org.usfirst.frc.team2521.robot.commands.automation.camera;
 
+import org.usfirst.frc.team2521.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class CameraLooper extends Command {
+	public CameraLooper() {
+		requires(Robot.camera);
+	}
+
 	@Override
 	protected void execute() {
 		Looper.getInstance().loop();
