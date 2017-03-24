589cbc0bb783f4b2ac7ea98cf81e16c4b36ca809
Alexandre Saveau <saveau.alexandre@gmail.com>
2017-03-23 19:53:03 -0700

    Make it clearer that DriveToBoiler.java isn't using DriveToBlob

--- src/org/usfirst/frc/team2521/robot/commands/automation/DriveToBoiler.java
+++ src/org/usfirst/frc/team2521/robot/commands/automation/DriveToBoiler.java
@@ -2,12 +2,14 @@ package org.usfirst.frc.team2521.robot.commands.automation;
 
 import org.usfirst.frc.team2521.robot.Robot;
 
+import edu.wpi.first.wpilibj.command.PIDCommand;
+
 import static org.usfirst.frc.team2521.robot.subsystems.Sensors.Camera;
 
 /**
  * This command drives to the correct spot to shoot from automatically.
  */
-public class DriveToBoiler extends DriveToBlob {
+public class DriveToBoiler extends PIDCommand {
 	private static final double P = 0.003;
 	private static final double I = 0;
 	private static final double D = 0;
@@ -17,6 +19,7 @@ public class DriveToBoiler extends DriveToBlob {
 
 	public DriveToBoiler() {
 		super(P, I, D);
+		requires(Robot.drivetrain);
 	}
 
 	@Override
