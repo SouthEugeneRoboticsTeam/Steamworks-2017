21bcff166cfc306c10d1211205319b4733a382f3
Alex Saveau <saveau.alexandre@gmail.com>
2017-03-24 10:09:53 -0700

    Make climber speed adjustable

--- src/org/usfirst/frc/team2521/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team2521/robot/subsystems/Climber.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team2521.robot.subsystems;
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.TalonControlMode;
 
+import org.usfirst.frc.team2521.robot.OI;
 import org.usfirst.frc.team2521.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -11,8 +12,6 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * Climber is the subsystem dedicated to the robot's rope climber.
  */
 public class Climber extends Subsystem {
-	private static final double CLIMBER_SPEED = -1;
-
 	private CANTalon master;
 	private CANTalon slave;
 
@@ -28,7 +27,7 @@ public class Climber extends Subsystem {
 	 * Runs the climber at a constant speed.
 	 */
 	public void runClimber() {
-		master.set(CLIMBER_SPEED);
+		master.set(-OI.getInstance().getRightStick().getY());
 		slave.set(RobotMap.CLIMBER_MASTER_MOTOR);
 	}
 
