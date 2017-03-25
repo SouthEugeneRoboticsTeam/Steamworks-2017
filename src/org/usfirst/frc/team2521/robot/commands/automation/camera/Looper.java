2c2ec999b93855db265f5b9c2044500a68b03d73
Alexandre Saveau <saveau.alexandre@gmail.com>
2017-03-25 08:22:26 -0700

    Update threasholds

--- src/org/usfirst/frc/team2521/robot/commands/automation/camera/Looper.java
+++ src/org/usfirst/frc/team2521/robot/commands/automation/camera/Looper.java
@@ -100,7 +100,7 @@ public final class Looper implements Runnable {
 		Preferences prefs = Preferences.getInstance();
 		Scalar lowerThreshold = new Scalar(
 				prefs.getInt("lower_b", 0),
-				prefs.getInt("lower_g", 20),
+				prefs.getInt("lower_g", 10),
 				prefs.getInt("lower_r", 0));
 		Scalar upperThreshold = new Scalar(
 				prefs.getInt("upper_b", 75),
